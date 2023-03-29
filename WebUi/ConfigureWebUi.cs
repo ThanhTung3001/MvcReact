cac5dfd0f4ff5963cfb6ed0b70c33ec971564aeb
ThanhTung3001 <tungle3001.itfreelancer@gmail.com>
2023-03-29 17:58:16 +0700

    commit

---
+++ WebUi/ConfigureWebUi.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Runtime.CompilerServices;
+
+namespace WebUi
+{
+    public this class ConfigureWebUi
+    {
+        public IServiceCollection AddConfigureWebUi(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
